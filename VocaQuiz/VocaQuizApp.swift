50a2756941690a5c384e590752bf1d1ea4ae0639
riataso <rarararai0070@gmail.com>
2024-05-19 21:03:12 +0900

    Initial Commit

---
+++ VocaQuiz/VocaQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocaQuizApp.swift
+//  VocaQuiz
+//
+//  Created by 佐藤来 on 2024/05/19.
+//
+
+import SwiftUI
+
+@main
+struct VocaQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
