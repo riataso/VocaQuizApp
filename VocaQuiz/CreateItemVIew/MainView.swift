31536df48b19b9a09b4c49376f893f991637cf3a
riataso <rarararai0070@gmail.com>
2024-10-07 22:10:40 +0900

    :wrench:フォルダ構成を機能ごとに分割

--- VocaQuiz/CreateItemVIew/MainView.swift
+++ VocaQuiz/CreateItemVIew/MainView.swift
@@ -9,21 +9,24 @@ struct MainView: View {
                 Button {
 
                 } label: {
-                    Text("問題を解く")
-                        .font(.title2)
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .frame(width: UIScreen.main.bounds.width * 2/3)
-                        .background(
-                            LinearGradient(gradient: Gradient(colors: [
-                                                    Color(red: 76/255, green: 175/255, blue: 80/255, opacity: 0.5),
-                                                    Color(red: 56/255, green: 142/255, blue: 60/255, opacity: 0.7)  
-                                                ]), startPoint: .topLeading, endPoint: .bottomTrailing)
-                        )
-                        .cornerRadius(10)
-                        .shadow(color: Color.black.opacity(0.5), radius: 10, x: 0, y: 4)
+                    NavigationLink(destination: SelectQuizTypeView()) {
+                        Text("問題を解く")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .frame(width: UIScreen.main.bounds.width * 2/3)
+                            .background(
+                                LinearGradient(gradient: Gradient(colors: [
+                                                        Color(red: 76/255, green: 175/255, blue: 80/255, opacity: 0.5),
+                                                        Color(red: 56/255, green: 142/255, blue: 60/255, opacity: 0.7)
+                                                    ]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                            )
+                            .cornerRadius(10)
+                            .shadow(color: Color.black.opacity(0.5), radius: 10, x: 0, y: 4)
+                    }
+
                 }
                 .padding(.horizontal, 16)
             }
